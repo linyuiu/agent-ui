da513af067bfad1c529618af37987c7000780e93
linyu <1754186906@qq.com>
2026-02-10 00:50:00 +0800

    change user permission

--- backend/app/config/__init__.py
+++ backend/app/config/__init__.py
@@ -1,4 +1,3 @@
 from . import settings
-from .settings import *  # noqa: F401,F403
 
-__all__ = ["settings", "_load_env"]
+__all__ = ["settings"]
--- backend/app/config/config.py
+++
@@ -1,1 +0,0 @@
-from .settings import *  # noqa: F401,F403
